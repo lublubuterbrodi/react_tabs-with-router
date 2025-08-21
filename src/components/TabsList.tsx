e41b270ac791fa475e2702e2809a7413c8b1fba0
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-08-21 15:57:17 +0300

    add task solution

--- src/components/TabsList.tsx
+++ src/components/TabsList.tsx
@@ -1,9 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { tabs } from '../types/Tab';
 
 export function TabsList({ activeId }: { activeId?: string }) {
-  const navigate = useNavigate();
-
   return (
     <div className="tabs is-boxed">
       <ul>
@@ -12,7 +10,6 @@ export function TabsList({ activeId }: { activeId?: string }) {
             key={t.id}
             data-cy="Tab"
             className={activeId === t.id ? 'is-active' : ''}
-            onClick={() => navigate(`/tabs/${t.id}`)}
           >
             <Link to={`/tabs/${t.id}`}>{t.title}</Link>
           </li>
